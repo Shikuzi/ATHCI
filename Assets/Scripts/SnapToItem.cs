343afe2547e65395845e2c3575a063ae3b0a60eb
Maarten Martens <maarten.martens@student.uhasselt.be>
2015-11-09 18:35:57 +0100

    Added dropdown menu with keyboard support untill we have gestures working. -F opens menu -Clicking and dragging moves menu. -Arrow keys give velocity to the content, we can use this technique to manipulate the menu with swipe gestures.

---
+++ Assets/Scripts/SnapToItem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class SnapToItem : MonoBehaviour {
+    public UIController controller;
+    public RectTransform content;
+    public RectTransform center;
+    private ScrollRect scroll;
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+
+    void init()
+    {
+        scroll = GetComponent<ScrollRect>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(controller.showDropDown && Mathf.Abs(scroll.velocity.x) < 20)
+        {
+            float distBetween = Mathf.Abs(content.GetChild(0).gameObject.GetComponent<RectTransform>().position.x - content.GetChild(1).gameObject.GetComponent<RectTransform>().position.x); // +  2 * padding
+            int min = -1;
+            float minDist = 99999;
+            
+            for(int i = 0; i < content.childCount; ++i)
+            {
+                GameObject b = content.GetChild(i).gameObject;
+                float curDist = Mathf.Abs(b.GetComponent<RectTransform>().position.x - center.position.x);
+                if (curDist < minDist)
+                {
+                    minDist = curDist;
+                    min = i;
+                }
+            }
+            
+            RectTransform cpos = content.GetComponent<RectTransform>();
+            float newX = (min * -distBetween) - (distBetween/2);
+            cpos.anchoredPosition = new Vector2(newX, cpos.anchoredPosition.y);
+        }
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+    Animator animationController;
+    public bool showDropDown = false;
+    public ScrollRect scroll;
+    
+	// Use this for initialization
+	void Start () {
+        animationController = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.F))
+        {
+            showDropDown = showDropDown ? false : true;
+            animationController.SetBool("ShowDropDown", showDropDown);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            scroll.velocity = new Vector2(-125f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            scroll.velocity = new Vector2(125f, 0f);
+        }
+    }
+}
