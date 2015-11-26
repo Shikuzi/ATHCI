7d1a7a946a81bdfd47de9d8034af6810dfeb11b3
Maarten Martens <maarten.martens@student.uhasselt.be>
2015-11-26 13:44:55 +0100

    Added some color Load assets from resource folder Cleaned up Book Case prefab Fixed dropdown buttons

---
+++ Assets/Scripts/ContentManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ContentManager : MonoBehaviour {
+    public GameObject itemPrefab;
+
+	// Use this for initialization
+	void Start () {
+        loadContent();
+    }
+
+    // Load all the models
+    void loadContent()
+    {
+        Object[] assets = Resources.LoadAll("Furniture/");
+        foreach (Object asset in assets)
+        {   
+            if(PrefabUtility.GetPrefabType(asset) != PrefabType.None)
+            {
+                Debug.Log(asset.name);
+            }
+            else
+            {
+                Debug.Log(asset.name + " is not a prefab.");
+            }
+        }
+    }
+}
