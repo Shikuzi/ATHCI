4dbb9314b16f5ba99c9d9314defe9dca8e4657e7
Yunus Altin <altin.yunus1@gmail.com>
2015-11-26 13:32:14 +0100

    gesturecontroller
    + grab
    + pointing

--- Assets/GestureController.cs
+++ Assets/GestureController.cs
@@ -7,6 +7,8 @@ public class GestureController : MonoBehaviour {
     // Use this for initialization
     bool inMenu;
     public UIController ui;
+    enum Mode { Grab, SwipeGesture, None};
+    Mode mode;
 	void Start () {
         controller = new Controller();
         controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
@@ -19,11 +21,37 @@ public class GestureController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Frame frame = controller.Frame();
+        HandList hands = frame.Hands;
+        Hand rightHand = frame.Hands.Rightmost;
+        Hand leftHand = frame.Hands.Leftmost;
+        Pointable knownPointable = leftHand.Pointables.Frontmost;
+
+        Debug.Log("pointable: " + knownPointable.ToString());
+
+        if (rightHand.GrabStrength == 1)
+        {
+            mode = Mode.Grab;
+
+            Vector handCenter = rightHand.PalmPosition;
+
+            float x = handCenter.x;
+            float y = handCenter.y;
+            float z = handCenter.z;
+            Debug.Log("GrabMode : x=" + x + " y=" + y + " z=" + z);
+
+            float pitch = rightHand.Direction.Pitch;
+            float yaw = rightHand.Direction.Yaw;
+            float roll = rightHand.PalmNormal.Roll;
+            //Debug.Log("GrabMode : pitch="+pitch + " yaw=" + yaw + " roll=" + roll);
+        }
+        
         GestureList gestures = frame.Gestures();
         foreach(Gesture g in gestures)
         {
             if(g.Type == Gesture.GestureType.TYPE_SWIPE)
             {
+                mode = Mode.SwipeGesture;
+                Debug.Log("SwipeMode");
                 SwipeGesture swipe = new SwipeGesture(g);
                 float speed = swipe.Speed;
                 if (swipe.Direction.y < -0.8)
