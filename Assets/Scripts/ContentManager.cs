5c6d198ae33ac5eed98c2fa4a022c754bef7da4b
Frank Erens <frank@synthi.net>
2015-12-15 10:10:35 +0100

    Fix a few merge issues

--- Assets/Scripts/ContentManager.cs
+++ Assets/Scripts/ContentManager.cs
@@ -17,18 +17,17 @@ public class ContentManager : MonoBehaviour {
     {
         itemArray = new ArrayList();
         UnityEngine.Object[] assets = Resources.LoadAll("Furniture/");
+        // UnityEngine.Object[] assets = {};
+
 
         foreach (UnityEngine.Object asset in assets)
         {
-            if (PrefabUtility.GetPrefabType(asset) == PrefabType.Prefab)
-            {
-
+            if (PrefabUtility.GetPrefabType(asset) == PrefabType.Prefab) {
                 Texture2D texture = null;
-                while (texture == null)
-{
+                // while (texture == null) {
                     texture = AssetPreview.GetAssetPreview(asset);
-                    System.Threading.Thread.Sleep(15);
-                }
+                //    System.Threading.Thread.Sleep(15);
+                // }
                 itemArray.Add(asset);
                 int items = itemArray.Count;
                 int row = (items / 7);
@@ -38,7 +37,8 @@ public class ContentManager : MonoBehaviour {
                 transform.GetChild(row).transform.GetChild(col).gameObject.GetComponent<Button>().enabled = true;
                 transform.GetChild(row).transform.GetChild(col).gameObject.GetComponent<Button>().GetComponent<ButtonInformation>().setInformation(row, col);
                 transform.GetChild(row).transform.GetChild(col).gameObject.GetComponent<Image>().enabled = true;
-                transform.GetChild(row).transform.GetChild(col).gameObject.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+                // transform.GetChild(row).transform.GetChild(col).gameObject.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+                break;
             }
             else
             {
